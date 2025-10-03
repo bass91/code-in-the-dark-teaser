f42ebe738b9c7d1c647dcd415423472219bb2e14
Sebastian Johansson <sebastian.johansson@mpyadigital.com>
2025-10-03 12:51:29 +0200

    offset the flashlight on mobile to prevent covering the light

--- app/components/Overlay.vue
+++ app/components/Overlay.vue
@@ -55,7 +55,7 @@ const transformY = computed(() => y.value - height.value / 2);
     align-items: center;
     pointer-events: none;
     place-self: center;
-    translate: var(--x) calc(var(--y));
+    translate: var(--x) calc(var(--y) - 60px);
     transition: radial-gradient 0.5s;
 }
 
